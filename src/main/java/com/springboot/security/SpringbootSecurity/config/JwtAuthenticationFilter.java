88dec52f9c3ba1519fa89b1084f25d18e5451f03
Pathum chinthaka <chinthakapeiris2002@gmail.com>
2023-10-12 15:22:48 +0530

    add jwt filer and dependency

--- src/main/java/com/springboot/security/SpringbootSecurity/config/JwtAuthenticationFilter.java
+++ src/main/java/com/springboot/security/SpringbootSecurity/config/JwtAuthenticationFilter.java
@@ -1,16 +1,36 @@
 package com.springboot.security.SpringbootSecurity.config;
 
+import com.springboot.security.SpringbootSecurity.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader=request.getHeader("Authorization");
+        final String jwtToken;
+        final String userEmail;
+        if(authHeader==null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        jwtToken=authHeader.substring(7);
+        userEmail=jwtService.extractUserName(jwtToken);
     }
 }
---
+++ src/main/java/com/springboot/security/SpringbootSecurity/service/JwtService.java
@@ -0,0 +1,11 @@
+package com.springboot.security.SpringbootSecurity.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUserName(String jwtToken) {
+        return null;
+    }
+}
